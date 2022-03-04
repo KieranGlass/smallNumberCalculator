2c6488ed94b5b62614b2fa71157d710e9d0adecc
KieranGlass <99490916+KieranGlass@users.noreply.github.com>
2022-03-04 10:02:49 +0100

    Add files via upload

---
+++ numberInfo/MathMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace numberInfo
+{
+    public class MathMethods
+    { 
+
+        public static int numPlus(int x, int y)
+            {
+                return x + y;
+            }
+        public static int numMinus(int x, int y)
+            {
+                return x - y;
+            }
+        public static int numMulti(int x, int y)
+            {
+                return x * y;
+            }
+        public static double numDiv(double x, double y)
+            {
+                return x / y;
+            }
+        public static double numSqu(double x, double y)
+            {
+            return Math.Sqrt(x + y);
+            }
+        
+        
+      
+    }
+
+}
---
+++ numberInfo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace numberInfo
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string first;
+            int num1;
+            string second;
+            int num2;
+         
+            Console.WriteLine("Enter two numbers! Don't use Zero or any non-numeric characters!");
+
+            Console.WriteLine("What is your first number!?");
+            first = Console.ReadLine();
+
+            while (!int.TryParse(first, out num1) || first == "0")
+            {
+                Console.WriteLine("Cant you read! NO 0'S or LETTERS, What is your first Number!?");
+                first = Console.ReadLine();
+            }
+            num1 = Convert.ToInt32(first);
+            
+            Console.WriteLine("What is your second number!?");
+            second = Console.ReadLine();
+
+            while (!int.TryParse(second, out num2) || second  == "0")
+            {
+                Console.WriteLine("Cant you read! NO 0'S or LETTERS, What is your second Number!?");
+                second = Console.ReadLine();
+            }
+
+            num2 = Convert.ToInt32(second);
+
+            Console.WriteLine("Ready. Press Enter!?"); Console.ReadLine();                       
+                      
+            Console.WriteLine("The Results are in!");             
+            Console.WriteLine(first + " + " + second + " = " + MathMethods.numPlus(num1, num2));
+            Console.WriteLine(first + " - " + second + " = " + MathMethods.numMinus(num1, num2));
+            Console.WriteLine(first + " * " + second + " = " + MathMethods.numMulti(num1, num2));
+            Console.WriteLine(first + " / " + second + " = " + MathMethods.numDiv(num1, num2));
+            Console.WriteLine("The Square Root of " + first + " and " + second  + " added together = " + MathMethods.numSqu(num1, num2));
+            Console.ReadLine();
+        }
+    }
+}
